6cc186a00b746ac1165baa2e09fcc5760098f500
kai <147840017+kairgozhin-s@users.noreply.github.com>
2025-05-16 00:27:24 +0100

    Bug Fixes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-    return config;
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['raw.githubusercontent.com', 'assets.coingecko.com'],
   },
-  transpilePackages: [
-    '@solana/wallet-adapter-base',
-    '@solana/wallet-adapter-react',
-    '@solana/wallet-adapter-react-ui',
-    '@solana/wallet-adapter-wallets',
-  ],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
